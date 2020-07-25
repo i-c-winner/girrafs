2027a65c316a6ee0315c0dc0cffd08f5293e13dc
i-c-winner <d-belousov@hotmail.com>
2020-07-25 23:58:31 +0300

    Updates

---
+++ precache-manifest.03ac277d92cae4334b220b37281da245.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f06fa258987b6cf4fa5b982ab4b710",
+    "url": "/girrafs/index.html"
+  },
+  {
+    "revision": "b867b46fb18be103e6d4",
+    "url": "/girrafs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bced652090161c7de76d",
+    "url": "/girrafs/static/css/main.e94ccaa5.chunk.css"
+  },
+  {
+    "revision": "b867b46fb18be103e6d4",
+    "url": "/girrafs/static/js/2.cb3bb552.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/girrafs/static/js/2.cb3bb552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bced652090161c7de76d",
+    "url": "/girrafs/static/js/main.eaedaf6a.chunk.js"
+  },
+  {
+    "revision": "e27af6721ff02701032b",
+    "url": "/girrafs/static/js/runtime-main.511259da.js"
+  },
+  {
+    "revision": "44f83e15f0fe92332fd3a98846358dc8",
+    "url": "/girrafs/static/media/bell.44f83e15.svg"
+  },
+  {
+    "revision": "68b22e62a1c4886051e4faf941b2d938",
+    "url": "/girrafs/static/media/libra.68b22e62.svg"
+  },
+  {
+    "revision": "9cef79fbfc4f538d16561ee01aec131c",
+    "url": "/girrafs/static/media/line.9cef79fb.svg"
+  },
+  {
+    "revision": "ac1be5de06fdbb5d9f9363e6fc4ee985",
+    "url": "/girrafs/static/media/sex.ac1be5de.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2adb35cbf6e9c56a2ec9a1d78386e315.js"
+  "/girrafs/precache-manifest.03ac277d92cae4334b220b37281da245.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/girrafs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
