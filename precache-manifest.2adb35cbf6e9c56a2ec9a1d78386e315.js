b2465fd140e4b12975c7906d4090f8c4c7f5ded4
i-c-winner <d-belousov@hotmail.com>
2020-07-25 16:04:29 +0300

    Updates

---
+++ precache-manifest.2adb35cbf6e9c56a2ec9a1d78386e315.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41072c281cf8f874c29a8e9d4a5de26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f04af004c20696f9130",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c860a9b810117f67a2f4",
+    "url": "/static/css/main.fece6f1e.chunk.css"
+  },
+  {
+    "revision": "0f04af004c20696f9130",
+    "url": "/static/js/2.6997dbf3.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/static/js/2.6997dbf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c860a9b810117f67a2f4",
+    "url": "/static/js/main.392f9994.chunk.js"
+  },
+  {
+    "revision": "7b8a55bb17e31247ef62",
+    "url": "/static/js/runtime-main.0c1081c7.js"
+  },
+  {
+    "revision": "44f83e15f0fe92332fd3a98846358dc8",
+    "url": "/static/media/bell.44f83e15.svg"
+  },
+  {
+    "revision": "68b22e62a1c4886051e4faf941b2d938",
+    "url": "/static/media/libra.68b22e62.svg"
+  },
+  {
+    "revision": "9cef79fbfc4f538d16561ee01aec131c",
+    "url": "/static/media/line.9cef79fb.svg"
+  },
+  {
+    "revision": "ac1be5de06fdbb5d9f9363e6fc4ee985",
+    "url": "/static/media/sex.ac1be5de.svg"
+  }
+]);
